8822ce1912d27d9df2310ef37cd58d442818ace4
Selçuk Temizsoy <selcuk@kloia.com>
2021-09-14 16:42:45 +0300

    First commit

---
+++ src/test/java/com/epias/simple_tests/DemoTest.java
@@ -0,0 +1,14 @@
+package com.epias.simple_tests;
+
+import org.junit.jupiter.api.Test;
+
+public class DemoTest {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+    @Test
+    public void test(){
+        System.out.println("hello world");
+    }
+}
