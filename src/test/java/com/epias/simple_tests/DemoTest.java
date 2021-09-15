d6636f25862b530d020d524b06fd6e92d08a3f33
Selçuk Temizsoy <selcuk@kloia.com>
2021-09-15 12:52:47 +0300

    Second commit

--- src/test/java/com/epias/simple_tests/DemoTest.java
+++ src/test/java/com/epias/simple_tests/DemoTest.java
@@ -1,6 +1,9 @@
 package com.epias.simple_tests;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+
 
 public class DemoTest {
     public static void main(String[] args) {
@@ -9,6 +12,7 @@ public class DemoTest {
 
     @Test
     public void test(){
-        System.out.println("hello world");
+        Assertions.assertEquals("2", "2");
+
     }
 }
